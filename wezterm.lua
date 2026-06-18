8202e5e1b59a273b871b86789399a5b5a2c48020
Federico Herrera <84097984+FdHerrera@users.noreply.github.com>
2026-06-18 16:53:47 -0300

    feat(keys): add some useful keys

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,22 @@ function scheme_for_appearance(appearance)
 	end
 end
 
-return {
-	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+local config = wezterm.config_builder()
+
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+local act = wezterm.action
+
+config.keys = {
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 }
+for i = 1, 8 do
+	-- ALT + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = act.ActivateTab(i - 1),
+	})
+end
+
+return config
